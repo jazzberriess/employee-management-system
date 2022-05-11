d8cd46b4e28dcc90aeedc13216c6c4385c904097
Jazzberries <anime.flick.chick@gmail.com>
2022-05-11 18:45:02 +1000

    Added required modules to server.js, added middleware for espress, set up server ports and listening.

---
+++ server.js
@@ -0,0 +1,20 @@
+//Required modules
+const express = require("express");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+//Setting up port
+const PORT = process.env.PORT || 3001;
+
+//Setting variable for express
+const app = express();
+
+//Express middleware to read JSON and urlencoded
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Listening for the server
+app.listen(PORT, () => {
+    console.log(`We're live and running the server on port ${PORT}`)
+})
